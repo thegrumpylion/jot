73509ef74e0bce3094a151fb63cc69f9514d9baf
nikolas sepos <nikolas.sepos@platform.cy>
2022-11-02 23:26:29 +0200

    feat: add search command

--- cmd/search.go
+++ cmd/search.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+
+	"github.com/blugelabs/bluge"
+	"github.com/spf13/cobra"
+)
 
 type searchCommand struct {
 	*cobra.Command
@@ -10,6 +16,7 @@ var searchCmd = &searchCommand{
 	Command: &cobra.Command{
 		Use:  "search",
 		RunE: searchCmdRunE,
+		Args: cobra.ExactArgs(1),
 	},
 }
 
@@ -18,5 +25,48 @@ func init() {
 }
 
 func searchCmdRunE(cmd *cobra.Command, args []string) error {
+
+	// bluge default config
+	config := bluge.DefaultConfig(".jot/index")
+
+	// set default search field to "content"
+	config.DefaultSearchField = "content"
+
+	// new match query on args[0]
+	query := bluge.NewMatchQuery(args[0])
+
+	// open index reader
+	reader, err := bluge.OpenReader(config)
+	if err != nil {
+		return err
+	}
+
+	// close reader
+	defer reader.Close()
+
+	// search request all matches
+	request := bluge.NewAllMatches(query)
+
+	// search
+	results, err := reader.Search(context.Background(), request)
+	if err != nil {
+		return err
+	}
+
+	// print results
+	next, err := results.Next()
+	for err == nil && next != nil {
+		next.VisitStoredFields(func(field string, value []byte) bool {
+			if field == "_id" {
+				fmt.Println(string(value))
+			}
+			return true
+		})
+		if err != nil {
+			return err
+		}
+		next, err = results.Next()
+	}
+
 	return nil
 }
